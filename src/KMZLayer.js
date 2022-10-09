63a3a3f73d2255bcb980a6532bddee785d1e2b0c
handymenny <handymenny@outlook.com>
2022-10-09 15:39:37 +0200

    Revert "update togeojson"
    This reverts commit 04e68330701eacb493bcf0a929b265262910a741.

--- src/KMZLayer.js
+++ src/KMZLayer.js
@@ -206,10 +206,7 @@ export const KMZLayer = L.KMZLayer = L.FeatureGroup.extend({
 			},
 			interactive: this.options.interactive,
 			filter: (feature) => {
-				// Filter out points without valid icon or with invalid coordinates
-				return feature.geometry.type !== "Point" ||
-					(feature.properties.icon || feature.properties["icon-scale"])
-					&& !isNaN(feature.geometry.coordinates[0]);
+				return feature.geometry.type !== "Point" || (feature.properties.icon || feature.properties["icon-scale"] != 0) && !isNaN(feature.geometry.coordinates[0]);
 			}
 		});
 		// parse GroundOverlays
